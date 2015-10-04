a56a966c8afb344f8c3551fdb8c224789070030f
Ardi <ardihamza@gmail.com>
2015-10-03 20:54:36 -0400

    fixed sending emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,8 +44,8 @@ Rails.application.configure do
                     :address        => "smtp.gmail.com",
                     :port           => 587,
                     :authentication => :plain,
-                    :user_name      => "irlpretest@gmail.com",
-                    :password       => "slimRD87",
+                    :user_name      => "albanianhiking@gmail.com",
+                    :password       => "ardithamzai",
                     :openssl_verify_mode  => 'none',
                     :enable_starttls_auto => true
   }
