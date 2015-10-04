c9501be074d48187374d6a7b90263e5f93728fba
Ardi <ardihamza@gmail.com>
2015-10-04 01:43:14 -0400

    fully integrated flickr API

--- app/helpers/flickr_helper.rb
+++ app/helpers/flickr_helper.rb
@@ -1,12 +1,12 @@
 module FlickrHelper  
-  def user_photos(user_id, photo_count = 12)
-    flickr = Flickr.new(File.join(RAILS_ROOT, 'config', 'flickr.yml'))
+  def user_photos(user_id, photo_count = 40)
+    flickr = Flickr.new(File.join(Rails.root, 'config', 'flickr.yml'))
     flickr.photos.search(:user_id => user_id).values_at(0..(photo_count - 1))
   end
   
-  def render_flickr_sidebar_widget(user_id, photo_count, columns)
+  def render_flickr_sidebar_widget(user_id, photo_count = 40, columns = 4)
     begin
-      photos = user_photos(user_id, photo_count).in_groups_of(2)
+      photos = user_photos(user_id, photo_count).in_groups_of(4)
       render :partial => '/flickr/sidebar_widget', :locals => { :photos => photos }
     rescue Exception
       render :partial => '/flickr/unavailable'
--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-require 'flickr_fu'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+require 'flickr_fu'
